f38e27244bb599acbf5cd509d129580df50aef44
HehMeah <ilia.glushkovv@gmail.com>
2023-06-17 00:51:45 +0300

    almost done, only need get image back))

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from telegram import Update, ReplyKeyboardMarkup, Bot, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler
+import aiohttp
 
 url = "http://127.0.0.1:7860"
-bot_token = "6063429082:AAFZLJiFXtI6Kotb7FZysNM5RxiiKX3qDEY"
+bot_token = ""
 
 menu = [
-    ["First setup / Первая настройка"],
+    ["Generate!"],
     ["Guide / Гайд",  "Presets"],
     ["Sampler", "Sampling steps / Качество"],
     ["Resolution / Разрешение", "Number of pictures / Кол-во изображений"],
@@ -23,30 +24,6 @@ guide_sections = [
 # Callback
 NEXT, PREVIOUS = range(2)
 
-payload = {
-  "enable_hr": "false",
-  "denoising_strength": 0,
-  "firstphase_width": 0,
-  "firstphase_height": 0,
-  "hr_scale": 2,
-  "hr_upscaler": "",
-  "prompt": "Cute dog paly with mango, on mango island",
-  "seed": -1,
-  "batch_size": 1,
-  "steps": 50,
-  "cfg_scale": 7,
-  "width": 512,
-  "height": 512,
-  "negative_prompt": "string",
-  "override_settings": {},
-  "override_settings_restore_afterwards": "true",
-  "sampler_index": "Euler",
-  "send_images": "true",
-  "save_images": "true",
-}
-
-
-
 
 def create_menu(index):
     menu = []
@@ -60,8 +37,8 @@ def create_menu(index):
 
 
 inline_menu_sampler = [
-    [InlineKeyboardButton("Euler a", callback_data='Euler'), InlineKeyboardButton("DPM2 Karras", callback_data='DPM')],
-    [InlineKeyboardButton("DPM ++2M Karras", callback_data='DPM2'), InlineKeyboardButton("DPM++ SDE Karras", callback_data='70')]
+    [InlineKeyboardButton("Euler", callback_data='Euler'), InlineKeyboardButton("DPM2 Karras", callback_data='DPM2 Karras')],
+    [InlineKeyboardButton("DPM ++2M Karras", callback_data='DPM ++2M Karras'), InlineKeyboardButton("DPM++ SDE Karras", callback_data='DPM++ SDE Karras')]
 ]
 
 inline_menu_num_pic = [
@@ -76,8 +53,8 @@ inline_menu_quality = [
 
 
 inline_menu_res = [
-    [InlineKeyboardButton("Set to 768x512", callback_data='res_vertical'), InlineKeyboardButton("Set to 512x768", callback_data='res_horizontal')],
-    [InlineKeyboardButton("Set to 768x768", callback_data='res_square')]
+    [InlineKeyboardButton("Set to 768x512", callback_data='vertical'), InlineKeyboardButton("Set to 512x768", callback_data='horizontal')],
+    [InlineKeyboardButton("Set to 768x768", callback_data='square')]
 ]
 
 inline_menu_seed = [
@@ -87,13 +64,12 @@ inline_menu_seed = [
 
 
 inline_menu_upscaler = [
-    [InlineKeyboardButton("R-ESRGAN 4x+ Anime6B" + " 🎲", callback_data='rea')],
-    [InlineKeyboardButton("ESRGAN_4x" + " ♻", callback_data='re')]
+    [InlineKeyboardButton("R-ESRGAN 4x+ Anime6B", callback_data='rea')],
+    [InlineKeyboardButton("ESRGAN_4x", callback_data='re')]
 ]
 
 inline_menu_upscale_to =[
-    [InlineKeyboardButton("Upscale x1.3", callback_data='x1.3')],
-    [InlineKeyboardButton("upscale x1.5", callback_data='x1.5')],
+    [InlineKeyboardButton("Upscale x1.3", callback_data='x1.3'), InlineKeyboardButton("Upscale x1.5", callback_data='x1.5')],
     [InlineKeyboardButton("upscale x2", callback_data='x2')],
 
 ]
@@ -141,8 +117,10 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         context.user_data['section'] = 0
     elif update.message.text == "Resolution / Разрешение":
         await update.message.reply_text('Choose resolution and aspect ratio', reply_markup=inline_reply_markup_res)
+
     elif update.message.text == "Seed":
         await update.message.reply_text('Choose option', reply_markup=inline_reply_markup_seed)
+
     elif update.message.text == "CFG scale":
         await update.message.reply_text('Choose how strongly the picture will fit the context',
                                         reply_markup=inline_reply_markup_cfg)
@@ -151,10 +129,34 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 
     elif update.message.text == "Number of pictures / Кол-во изображений":
         await update.message.reply_text('Choose quantity', reply_markup=inline_reply_markup_num_pic)
+
     elif update.message.text == "Upscaler":
         await update.message.reply_text('Choose upscaler', reply_markup=inline_reply_markup_upscaler)
+
     elif update.message.text == "Upscale to":
         await update.message.reply_text('Choose multiplier', reply_markup=inline_reply_markup_upscale_to)
+
+    elif context.user_data.get('user_prompt', False):
+        context.user_data['prompt'] = update.message.text
+        context.user_data['user_prompt'] = False
+
+        await update.message.reply_text("Generating....")
+
+        payload = prepare_payload(context)
+        headers = {'accept': 'application/json'}
+        endpoint = f'{url}/sdapi/v1/txt2img'
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(endpoint, headers=headers, json=payload) as response:
+                if response.status == 200:
+                    await update.message.reply_text("Generation successful!")
+                else:
+                    await update.message.reply_text(f"Generation failed with status code {response.status}")
+        return
+
+    elif update.message.text == "Generate!":
+        context.user_data['user_prompt'] = True
+        await update.message.reply_text('Please type your prompt....')
 
     else:
         await update.message.reply_text(update.message.text)
@@ -191,22 +193,45 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await query.answer(f"You've selected {query.data} quality.")
         return
 
-    elif query.data in ['res_vertical', 'res_horizontal', 'res_square']:
-        context.user_data['resolution'] = query.data
-        await query.answer(f"You've selected {query.data}.")
+    elif query.data in ['vertical', 'horizontal', 'square']:
+        if query.data == 'vertical':
+            context.user_data['width'] = 512
+            context.user_data['height'] = 768
+        elif query.data == 'horizontal':
+            context.user_data['width'] = 768
+            context.user_data['height'] = 512
+        elif query.data == 'square':
+            context.user_data['width'] = 768
+            context.user_data['height'] = 768
+        await query.answer(f"You've selected {query.data} resolution.")
         return
 
     elif query.data in ['rea', 're']:
-        context.user_data['upscaler'] = query.data
+        if query.data == 'rea':
+            context.user_data['upscaler'] = "R-ESRGAN 4x+ Anime6B"
+        elif query.data == 're':
+            context.user_data['upscaler'] = "ESRGAN 4x"
         await query.answer(f"You've selected {query.data}.")
         return
 
     elif query.data in ['x1.3', 'x1.5', 'x2']:
-        context.user_data['upscale_to'] = query.data
+        if query.data == 'x1.3':
+            context.user_data['upscale_to'] = "1.3"
+            context.user_data['upscale_on'] = "true"
+        elif query.data == 'x1.5':
+            context.user_data['upscale_to'] = "1.5"
+            context.user_data['upscale_on'] = "true"
+        elif query.data == 'x2':
+            context.user_data['upscale_to'] = "2"
+            context.user_data['upscale_on'] = "true"
         await query.answer(f"You've selected {query.data}.")
         return
 
     elif query.data in ['return_prev', 'randomize']:
+        if query.data == 'return_prev':
+            context.user_data['seed'] = "-1" #Should be finished now just random,heh
+        elif query.data == 'randomize':
+            context.user_data['seed'] = "-1"
         context.user_data['seed'] = query.data
         await query.answer(f"You've selected {query.data}.")
         return
@@ -214,6 +239,19 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     elif query.data in ['2', '4', '6', '8']:
         context.user_data['num_pic'] = query.data
         await query.answer(f"You've selected {query.data}.")
+        return
+
+    elif query.data == 'GENERATE':
+        payload = prepare_payload(context)
+        headers = {'accept': 'application/json'}
+        endpoint = f'{url}/sdapi/v1/txt2img'
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(endpoint, headers=headers, json=payload) as response:
+                if response.status == 200:
+                    await update.message.reply_text("Generation successful!")
+                else:
+                    await update.message.reply_text(f"Generation failed with status code {response.status}")
         return
 
     await query.edit_message_text(
@@ -224,9 +262,27 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     context.user_data['section'] = current_section
 
 
-
-
-
+def prepare_payload(context):
+    user_data = context.user_data
+    payload = {
+        "enable_hr": user_data.get("upscale_on", "false"),
+        "prompt": user_data.get('prompt', "Dogs playing poker"),
+        "seed": user_data.get('seed', -1),
+        "batch_count": user_data.get('num_pic', 1),
+        "steps": user_data.get('quality', 50),
+        "cfg_scale": user_data.get('cfg', 7.5),
+        "width": user_data.get('width', 768),
+        "height": user_data.get('height', 512),
+        "negative_prompt": user_data.get('negative_prompt', "low resolution, bad anatomy, bad hands, text, "
+                                        "error, missing fingers, extra digit, fewer digits, cropped, worst quality, "
+                                        "low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry"),
+        "sampler_index": user_data.get('sampler', "Euler"),
+        "send_images": user_data.get('send_images', "true"),
+        "save_images": user_data.get('save_images', "true"),
+        "hr_upscaler": user_data.get("upscaler", "none"),
+        "hr_scale": user_data.get("upscale_to", 0)
+    }
+    return payload
 
 
 app = ApplicationBuilder().token(bot_token).build()
