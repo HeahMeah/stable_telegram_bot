1f6d21018959845d451f04e7f32d8fd8bad5e812
HehMeah <ilia.glushkovv@gmail.com>
2023-06-16 20:08:36 +0300

    button handler almoast done

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from telegram import Update, ReplyKeyboardMarkup, Bot, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler
 
-
-bot_token = "TOKEN"
+url = "http://127.0.0.1:7860"
+bot_token = "6063429082:AAFZLJiFXtI6Kotb7FZysNM5RxiiKX3qDEY"
 
 menu = [
+    ["First setup / Первая настройка"],
     ["Guide / Гайд",  "Presets"],
     ["Sampler", "Sampling steps / Качество"],
     ["Resolution / Разрешение", "Number of pictures / Кол-во изображений"],
@@ -22,6 +23,30 @@ guide_sections = [
 # Callback
 NEXT, PREVIOUS = range(2)
 
+payload = {
+  "enable_hr": "false",
+  "denoising_strength": 0,
+  "firstphase_width": 0,
+  "firstphase_height": 0,
+  "hr_scale": 2,
+  "hr_upscaler": "",
+  "prompt": "Cute dog paly with mango, on mango island",
+  "seed": -1,
+  "batch_size": 1,
+  "steps": 50,
+  "cfg_scale": 7,
+  "width": 512,
+  "height": 512,
+  "negative_prompt": "string",
+  "override_settings": {},
+  "override_settings_restore_afterwards": "true",
+  "sampler_index": "Euler",
+  "send_images": "true",
+  "save_images": "true",
+}
+
+
+
 
 def create_menu(index):
     menu = []
@@ -35,8 +60,8 @@ def create_menu(index):
 
 
 inline_menu_sampler = [
-    [InlineKeyboardButton("Euler a", callback_data='20'), InlineKeyboardButton("DPM2 Karras", callback_data='35')],
-    [InlineKeyboardButton("DPM ++2M Karras", callback_data='50'), InlineKeyboardButton("DPM++ SDE Karras", callback_data='70')]
+    [InlineKeyboardButton("Euler a", callback_data='Euler'), InlineKeyboardButton("DPM2 Karras", callback_data='DPM')],
+    [InlineKeyboardButton("DPM ++2M Karras", callback_data='DPM2'), InlineKeyboardButton("DPM++ SDE Karras", callback_data='70')]
 ]
 
 inline_menu_num_pic = [
@@ -60,6 +85,20 @@ inline_menu_seed = [
     [InlineKeyboardButton("Return previous seed" + " ♻", callback_data='return_prev')]
 ]
 
+
+inline_menu_upscaler = [
+    [InlineKeyboardButton("R-ESRGAN 4x+ Anime6B" + " 🎲", callback_data='rea')],
+    [InlineKeyboardButton("ESRGAN_4x" + " ♻", callback_data='re')]
+]
+
+inline_menu_upscale_to =[
+    [InlineKeyboardButton("Upscale x1.3", callback_data='x1.3')],
+    [InlineKeyboardButton("upscale x1.5", callback_data='x1.5')],
+    [InlineKeyboardButton("upscale x2", callback_data='x2')],
+
+]
+
+
 inline_menu_cfg = [
     [InlineKeyboardButton("Set to 6", callback_data='cfg_6'), InlineKeyboardButton("Set to 6.5", callback_data='cfg_6.5')],
     [InlineKeyboardButton("Set to 7", callback_data='cfg_7'), InlineKeyboardButton("Set to 7.5", callback_data='cfg_7.5')],
@@ -77,6 +116,8 @@ inline_reply_markup_seed = InlineKeyboardMarkup(inline_menu_seed)
 inline_reply_markup_cfg = InlineKeyboardMarkup(inline_menu_cfg)
 inline_reply_markup_num_pic = InlineKeyboardMarkup(inline_menu_num_pic)
 inline_reply_markup_sampler = InlineKeyboardMarkup(inline_menu_sampler)
+inline_reply_markup_upscaler = InlineKeyboardMarkup(inline_menu_upscaler)
+inline_reply_markup_upscale_to = InlineKeyboardMarkup(inline_menu_upscale_to)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -107,8 +148,14 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
                                         reply_markup=inline_reply_markup_cfg)
     elif update.message.text == "Sampler":
         await update.message.reply_text('Choose sampler', reply_markup=inline_reply_markup_sampler)
+
     elif update.message.text == "Number of pictures / Кол-во изображений":
         await update.message.reply_text('Choose quantity', reply_markup=inline_reply_markup_num_pic)
+    elif update.message.text == "Upscaler":
+        await update.message.reply_text('Choose upscaler', reply_markup=inline_reply_markup_upscaler)
+    elif update.message.text == "Upscale to":
+        await update.message.reply_text('Choose multiplier', reply_markup=inline_reply_markup_upscale_to)
+
     else:
         await update.message.reply_text(update.message.text)
 
@@ -128,6 +175,46 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             reply_markup=reply_markup
         )
         return
+    elif query.data in ['cfg_6', 'cfg_6.5', 'cfg_7', 'cfg_7.5', 'cfg_8', 'cfg_8.5', 'cfg_9', 'cfg_9.5',
+                        'cfg_10', 'cfg_10.5', 'cfg_11', 'cfg_11.5', 'cfg_12', 'cfg_12.5', 'cfg_13']:
+        context.user_data['cfg'] = query.data
+        await query.answer(f"You've selected {query.data}.")
+        return
+
+    elif query.data in ['Euler', 'DPM2 Karras', 'DPM ++2M Karras', 'DPM++ SDE Karras']:
+        context.user_data['sampler'] = query.data
+        await query.answer(f"You've selected {query.data}.")
+        return
+
+    elif query.data in ['20', '35', '50', '70']:
+        context.user_data['quality'] = query.data
+        await query.answer(f"You've selected {query.data} quality.")
+        return
+
+    elif query.data in ['res_vertical', 'res_horizontal', 'res_square']:
+        context.user_data['resolution'] = query.data
+        await query.answer(f"You've selected {query.data}.")
+        return
+
+    elif query.data in ['rea', 're']:
+        context.user_data['upscaler'] = query.data
+        await query.answer(f"You've selected {query.data}.")
+        return
+
+    elif query.data in ['x1.3', 'x1.5', 'x2']:
+        context.user_data['upscale_to'] = query.data
+        await query.answer(f"You've selected {query.data}.")
+        return
+
+    elif query.data in ['return_prev', 'randomize']:
+        context.user_data['seed'] = query.data
+        await query.answer(f"You've selected {query.data}.")
+        return
+
+    elif query.data in ['2', '4', '6', '8']:
+        context.user_data['num_pic'] = query.data
+        await query.answer(f"You've selected {query.data}.")
+        return
 
     await query.edit_message_text(
         guide_sections[current_section],
@@ -135,6 +222,11 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         reply_markup=create_menu(current_section)
     )
     context.user_data['section'] = current_section
+
+
+
+
+
 
 
 app = ApplicationBuilder().token(bot_token).build()
